69771eebe4158f3becda437ca7b1b7e1008b5fd5
Sviatoslav Nakonechnyi <dielok4@gmail.com>
2024-04-05 01:37:41 +0300

    fix design

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,11 @@
 
 function Footer() {
   return (
-    <footer className="mt-[128px] flex flex-col justify-center ml-[-20px]">
+    <footer className="mt-[128px] flex flex-col justify-center ml-[-32px]">
       <div className="w-[1776px] h-[152px] rounded-[12px] bg-[#04F06A] flex items-center justify-center">
         <h2 className="text-[78px] font-[600] raleway opacity-[0.8]">Збережімо домівку разом!</h2>
       </div>
-      <div className="ml-[-170px]">
+      <div className="ml-[0px]">
         <img src="./image/Mask group.svg" alt="photo" className="w-[508px] h-[467px] m-auto mt-[39px]" />
       </div>
     </footer>
